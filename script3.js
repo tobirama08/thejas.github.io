31240e0591f5b823aa4e91d03dee443b16ea9303
Thejas <165163801+tobirama08@users.noreply.github.com>
2024-11-03 12:29:57 +0530

    Delete script3.js

--- script3.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.location.pathname.includes('index.html')) {
-        alert("Welcome to the Bio-data page!");
-    } else if (window.location.pathname.includes('resume.html')) {
-        alert("Welcome to the Resume page!");
-    }
-});
